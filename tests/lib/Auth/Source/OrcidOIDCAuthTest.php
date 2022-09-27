6282963ae6e94412f6f0a88532771e6f0a07ac2a
gary_windham <windhamg@gmail.com>
2022-09-27 08:26:28 -0700

    replace json_decode lines with PHP arrays; fix phpcs errors

--- tests/lib/Auth/Source/OrcidOIDCAuthTest.php
+++ tests/lib/Auth/Source/OrcidOIDCAuthTest.php
@@ -23,13 +23,162 @@ class OrcidOIDCAuthTest extends TestCase
     {
         return [
             // no email addresses
-            [json_decode('{"last-modified-date":null,"email":[],"path":"/0000-0000-0000-0000/email"}', true), null],
+            [
+                [
+                    "last-modified-date" => null,
+                    "email" => [],
+                    "path" => "/0000-0000-0000-0000/email"
+                ],
+                null
+            ],
             // no primary, but one non-primary
-            [json_decode('{"last-modified-date":{"value":1664233445808},"email":[{"created-date":{"value":1661900382942},"last-modified-date":{"value":1664233445808},"source":{"source-orcid":{"uri":"https://orcid.org/0000-0000-0000-0000","path":"0000-0000-0000-0000","host":"orcid.org"},"source-client-id":null,"source-name":{"value":"Test User"},"assertion-origin-orcid":null,"assertion-origin-client-id":null,"assertion-origin-name":null},"email":"non-primary@example.org","path":null,"visibility":"public","verified":true,"primary":false,"put-code":null}],"path":"/0000-0000-0000-0000/email"}', true), 'non-primary@example.org'],
+            [
+                [
+                    "last-modified-date" => [
+                        "value" => 1664233445808
+                    ],
+                    "email" => [
+                        [
+                            "created-date" => [
+                                "value" => 1661900382942
+                            ],
+                            "last-modified-date" => [
+                                "value" => 1664233445808
+                            ],
+                            "source" => [
+                                "source-orcid" => [
+                                    "uri" => "https =>//orcid.org/0000-0000-0000-0000",
+                                    "path" => "0000-0000-0000-0000",
+                                    "host" => "orcid.org"
+                                ],
+                                "source-client-id" => null,
+                                "source-name" => [
+                                    "value" => "Test User"
+                                ],
+                                "assertion-origin-orcid" => null,
+                                "assertion-origin-client-id" => null,
+                                "assertion-origin-name" => null
+                            ],
+                            "email" => "non-primary@example.org",
+                            "path" => null,
+                            "visibility" => "public",
+                            "verified" => true,
+                            "primary" => false,
+                            "put-code" => null
+                        ]
+                    ],
+                    "path" => "/0000-0000-0000-0000/email"
+                ],
+                "non-primary@example.org"
+            ],
             // primary and non-primary
-            [json_decode('{"last-modified-date":{"value":1664233809699},"email":[{"created-date":{"value":1487980758777},"last-modified-date":{"value":1664233809699},"source":{"source-orcid":{"uri":"https://orcid.org/0000-0000-0000-0000","path":"0000-0000-0000-0000","host":"orcid.org"},"source-client-id":null,"source-name":{"value":"Test User"},"assertion-origin-orcid":null,"assertion-origin-client-id":null,"assertion-origin-name":null},"email":"non-primary@example.org","path":null,"visibility":"public","verified":true,"primary":false,"put-code":null},{"created-date":{"value":1661900382942},"last-modified-date":{"value":1664233445808},"source":{"source-orcid":{"uri":"https://orcid.org/0000-0000-0000-0000","path":"0000-0000-0000-0000","host":"orcid.org"},"source-client-id":null,"source-name":{"value":"Test User"},"assertion-origin-orcid":null,"assertion-origin-client-id":null,"assertion-origin-name":null},"email":"primary@example.org","path":null,"visibility":"public","verified":true,"primary":true,"put-code":null}],"path":"/0000-0000-0000-0000/email"}', true), 'primary@example.org'],
+            [
+                [
+                    "last-modified-date" => [
+                        "value" => 1664233809699
+                    ],
+                    "email" => [
+                        [
+                            "created-date" => [
+                                "value" => 1487980758777
+                            ],
+                            "last-modified-date" => [
+                                "value" => 1664233809699
+                            ],
+                            "source" => [
+                                "source-orcid" => [
+                                    "uri" => "https =>//orcid.org/0000-0000-0000-0000",
+                                    "path" => "0000-0000-0000-0000",
+                                    "host" => "orcid.org"
+                                ],
+                                "source-client-id" => null,
+                                "source-name" => [
+                                    "value" => "Test User"
+                                ],
+                                "assertion-origin-orcid" => null,
+                                "assertion-origin-client-id" => null,
+                                "assertion-origin-name" => null
+                            ],
+                            "email" => "non-primary@example.org",
+                            "path" => null,
+                            "visibility" => "public",
+                            "verified" => true,
+                            "primary" => false,
+                            "put-code" => null
+                        ],
+                        [
+                            "created-date" => [
+                                "value" => 1661900382942
+                            ],
+                            "last-modified-date" => [
+                                "value" => 1664233445808
+                            ],
+                            "source" => [
+                                "source-orcid" => [
+                                    "uri" => "https =>//orcid.org/0000-0000-0000-0000",
+                                    "path" => "0000-0000-0000-0000",
+                                    "host" => "orcid.org"
+                                ],
+                                "source-client-id" => null,
+                                "source-name" => [
+                                    "value" => "Test User"
+                                ],
+                                "assertion-origin-orcid" => null,
+                                "assertion-origin-client-id" => null,
+                                "assertion-origin-name" => null
+                            ],
+                            "email" => "primary@example.org",
+                            "path" => null,
+                            "visibility" => "public",
+                            "verified" => true,
+                            "primary" => true,
+                            "put-code" => null
+                        ]
+                    ],
+                    "path" => "/0000-0000-0000-0000/email"
+                ],
+                "primary@example.org"
+            ],
             // only primary
-            [json_decode('{"last-modified-date":{"value":1664233445808},"email":[{"created-date":{"value":1661900382942},"last-modified-date":{"value":1664233445808},"source":{"source-orcid":{"uri":"https://orcid.org/0000-0000-0000-0000","path":"0000-0000-0000-0000","host":"orcid.org"},"source-client-id":null,"source-name":{"value":"Test User"},"assertion-origin-orcid":null,"assertion-origin-client-id":null,"assertion-origin-name":null},"email":"primary@example.org","path":null,"visibility":"public","verified":true,"primary":true,"put-code":null}],"path":"/0000-0000-0000-0000/email"}', true), 'primary@example.org']
+            [
+                [
+                    "last-modified-date" => [
+                        "value" => 1664233445808
+                    ],
+                    "email" => [
+                        [
+                            "created-date" => [
+                                "value" => 1661900382942
+                            ],
+                            "last-modified-date" => [
+                                "value" => 1664233445808
+                            ],
+                            "source" => [
+                                "source-orcid" => [
+                                    "uri" => "https =>//orcid.org/0000-0000-0000-0000",
+                                    "path" => "0000-0000-0000-0000",
+                                    "host" => "orcid.org"
+                                ],
+                                "source-client-id" => null,
+                                "source-name" => [
+                                    "value" => "Test User"
+                                ],
+                                "assertion-origin-orcid" => null,
+                                "assertion-origin-client-id" => null,
+                                "assertion-origin-name" => null
+                            ],
+                            "email" => "primary@example.org",
+                            "path" => null,
+                            "visibility" => "public",
+                            "verified" => true,
+                            "primary" => true,
+                            "put-code" => null
+                        ]
+                    ],
+                    "path" => "/0000-0000-0000-0000/email"
+                ],
+                "primary@example.org"
+            ]
         ];
     }
 
@@ -56,7 +205,15 @@ class OrcidOIDCAuthTest extends TestCase
 
         $mockRequest = $this->createMock(RequestInterface::class);
         $mock->method('getAuthenticatedRequest')
-            ->with('GET', strtr('https://pub.orcid.org/v3.0/@orcid/email', ['@orcid' => $state['Attributes']['oidc.sub'][0]]), $token, ['headers' => ['Accept' => 'application/json']])
+            ->with(
+                'GET',
+                strtr(
+                    'https://pub.orcid.org/v3.0/@orcid/email',
+                    ['@orcid' => $state['Attributes']['oidc.sub'][0]]
+                ),
+                $token,
+                ['headers' => ['Accept' => 'application/json']]
+            )
             ->willReturn($mockRequest);
 
         $mock->method('getParsedResponse')
