37a260c83d77b75091194992d3c5db3a9f5ffd53
Patrick Radtke <patrick@cirrusidentity.com>
2023-12-08 15:32:01 -0800

    Attempt to use ProcessingChain for authproc filter invocation
    Still need to test with authproc filters that performa redirect.

--- tests/config/config.php
+++ tests/config/config.php
@@ -5,3 +5,5 @@ $config = [];
 require "ssp2_0-config.php";
 $config['module.enable']['authoauth2'] = true;
 $config['baseurlpath'] = '/';
+$config['logging.handler'] = 'stderr';
+$config['logging.level'] = SimpleSAML\Logger::DEBUG;
