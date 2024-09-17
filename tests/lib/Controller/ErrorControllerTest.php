8b29dd5fedf039136b45edb6058686987c0634df
Ioannis Igoumenos <ioigoume@gmail.com>
2024-09-17 12:19:06 +0300

    ErrorController unit tests

---
+++ tests/lib/Controller/ErrorControllerTest.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Controller;
+
+use PHPUnit\Framework\TestCase;
+use SimpleSAML\Configuration;
+use SimpleSAML\Module\authoauth2\Controller\ErrorController;
+use SimpleSAML\XHTML\Template;
+use Symfony\Component\HttpFoundation\Request;
+
+class ErrorControllerTest extends TestCase
+{
+    private $controller;
+    private $config;
+
+    protected function setUp(): void
+    {
+        $this->config = Configuration::loadFromArray([
+           'baseurlpath' => '/',
+        ]);
+
+        $this->controller = new ErrorController($this->config);
+    }
+
+    public function testConsent(): void
+    {
+        $request = Request::create('/consent', 'GET');
+        $response = $this->controller->consent($request);
+
+        $this->assertInstanceOf(Template::class, $response);
+        $this->assertEquals('authoauth2:errors/consent.twig', $response->getTemplateName());
+    }
+}
