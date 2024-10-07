2741b0cac98c23857b61ddcba29956134a6365fd
Patrick Radtke <patrick@cirrusidentity.com>
2024-10-07 14:56:02 -0700

    Update base test ssp2 config file to be ssp 2.3 default

--- tests/config/config.php
+++ tests/config/config.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 $config = [];
 // require a vanilla SSP config
-require "ssp2_0-config.php";
+require "ssp2_3-config.php";
 $config['module.enable']['authoauth2'] = true;
 $config['baseurlpath'] = '/';
 $config['logging.handler'] = 'stderr';
